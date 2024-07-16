e28df71b126d3f2f02c133fea6597eb79688c4aa
Maros Karasek <maros.karasek@gmail.com>
2024-07-16 20:38:03 +0000

    changes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -10,9 +12,19 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+passport.initialize()
+passport.session()
 
 app.route('/').get((req, res) => {
-
+  res.render('index', { title: 'Hello', message: 'Please log in' })
 });
 
 const PORT = process.env.PORT || 3000;

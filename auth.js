75ca72f95c61fa546b3d18f71bca98c11d07f353
Maros Karasek <maros.karasek@gmail.com>
2024-07-17 20:14:17 +0000

    changes

--- auth.js
+++ auth.js
@@ -1,7 +1,9 @@
 const passport = require('passport');
 const { ObjectID } = require('mongodb');
 const LocalStrategy = require('passport-local');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt') 
+const GitHubStrategy = require('passport-github').Strategy;
+require('dotenv').config();
 
 module.exports = function (app, myDataBase) {
     passport.initialize()
@@ -28,4 +30,15 @@ module.exports = function (app, myDataBase) {
         return done(null, user);
       });
     }));
+
+    passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: 'https://3000-exiltruman-fccboilerpla-cmhxkox7p0x.ws-eu115.gitpod.io/auth/github/callback'
+      },
+        function(accessToken, refreshToken, profile, cb) {
+          console.log(profile);
+          //Database logic here with callback containing your user object
+        }
+      ));
 }

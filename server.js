c5a08794fe7821e99b0b53fe0ca2d98fa75e65ac
Maros Karasek <maros.karasek@gmail.com>
2024-07-18 20:12:57 +0000

    changes

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 const routes = require('./routes.js');
 const auth = require('./auth.js');
@@ -21,7 +26,9 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
+  name: 'express.sid',
+  store: store,
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -35,12 +42,46 @@ myDB(async client => {
   auth(app, myDataBase);
   routes(app, myDataBase);
 
+  io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: 'express.sid',
+      secret: process.env.SESSION_SECRET,
+      store: store,
+      success: onAuthorizeSuccess,
+      fail: onAuthorizeFail
+    })
+  );
+
   let currentUsers = 0;
   io.on('connection', (socket) => {
     ++currentUsers;
     io.emit('user count', currentUsers);
     console.log('A user has connected');
+    console.log('user ' + socket.request.user.username + ' connected');
+    socket.on('disconnect', () => {
+      console.log('A user has disconnected');
+      --currentUsers;
+      io.emit('user count', currentUsers);
+    });
   });
+  io.on('disconnect', () => {
+    /*anything you want to do on disconnect*/
+    --currentUsers;
+    io.emit('user count', currentUsers);
+  });
+
+  function onAuthorizeSuccess(data, accept) {
+    console.log('successful connection to socket.io');
+  
+    accept(null, true);
+  }
+  
+  function onAuthorizeFail(data, message, error, accept) {
+    if (error) throw new Error(message);
+    console.log('failed connection to socket.io:', message);
+    accept(null, false);
+  }
 
 }).catch(e => {
   app.route('/').get((req, res) => {

02a82cf639d8a54b05bc34a20011026a67a9c6c4
Maros Karasek <maros.karasek@gmail.com>
2024-07-18 20:18:23 +0000

    changes

--- server.js
+++ server.js
@@ -56,13 +56,21 @@ myDB(async client => {
   let currentUsers = 0;
   io.on('connection', (socket) => {
     ++currentUsers;
-    io.emit('user count', currentUsers);
+    io.emit('user', {
+      username: socket.request.user.username,
+      currentUsers,
+      connected: true
+    });
     console.log('A user has connected');
     console.log('user ' + socket.request.user.username + ' connected');
     socket.on('disconnect', () => {
       console.log('A user has disconnected');
       --currentUsers;
-      io.emit('user count', currentUsers);
+      io.emit('user', {
+        username: socket.request.user.username,
+        currentUsers,
+        connected: true
+      });
     });
   });
   io.on('disconnect', () => {
